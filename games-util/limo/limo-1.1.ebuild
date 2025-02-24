gentoo-mirror/parona-overlay

commit 3272c377386030da3a14eaa71502f739e880a93d

games-util/limo: include missing header numeric for gcc15
Signed-off-by: Alfred Wingate <parona@protonmail.com>

games-util/limo/limo-1.1.ebuild
@@ -48,6 +48,7 @@ BDEPEND="
 
 PATCHES=(
 	"${FILESDIR}"/limo-1.1-hookup-tests.ebuild
+	"${FILESDIR}"/limo-1.1-gcc-15-numeric.patch
 )
 
 src_configure() {

games-util/limo/files/limo-1.1-gcc15-numeric.patch
@@ -1,0 +1,13 @@
+diff --git a/src/core/reversedeployer.cpp b/src/core/reversedeployer.cpp
+index dc7723b..9ee76f9 100644
+--- a/src/core/reversedeployer.cpp
++++ b/src/core/reversedeployer.cpp
+@@ -6,6 +6,7 @@
+ #include <format>
+ #include <fstream>
+ #include <iostream>
++#include <numeric>
+ #include <ranges>
+ 
+ namespace sfs = std::filesystem;
+

commit 30b3a53e5f179a65dea7ee8adbd4ebc87771e95e

games-util/limo: fix patch typo
Fixes: 3272c377386030da3a14eaa71502f739e880a93d
Signed-off-by: Alfred Wingate <parona@protonmail.com>

games-util/limo/limo-1.1.ebuild
@@ -48,7 +48,7 @@ BDEPEND="
 
 PATCHES=(
 	"${FILESDIR}"/limo-1.1-hookup-tests.ebuild
-	"${FILESDIR}"/limo-1.1-gcc-15-numeric.patch
+	"${FILESDIR}"/limo-1.1-gcc15-numeric.patch
 )
 
 src_configure() {